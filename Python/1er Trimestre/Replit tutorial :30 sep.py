d821f2920c08f7e7f55c146f2b1065e3e7b7454d
WeskerGangus6156 <104381838+WeskerGangus6156@users.noreply.github.com>
2022-05-31 09:31:20 -0500

    Add files via upload

---
+++ Python/1er Trimestre/Replit tutorial :30 sep.py
@@ -0,0 +1,6 @@
+print("hola mundo")
+print("My name is Bruno Angulo Flores.")
+print("Im in ninth grade.")
+print("My group is 9th D.")
+print("I have various favorite foods Im going to say two tacos de cabeza, sushi and chilaquies.")
+print("Mr Im very excited to see the new movie of venom because is going to appear carnage")
